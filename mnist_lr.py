0623c61a80e317e46301b4015eadf35c2aea2a34
Robin Bhattacharya <robinbhattacharya18@gmail.com>
2017-10-29 19:42:26 -0700

    Update mnist_lr.py

--- mnist_lr.py
+++ mnist_lr.py
@@ -12,31 +12,43 @@ learning_rate = 0.5
 def main(_):
 	#The line below reads in the MNSIT data into the folder MNIST_data/
 	mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
-
+        ###Enter code to define the necessary placeholders we need for this program###
+	#SOLUTION:
+	
 	#The line below creates the placeholders for the label and features
 	#We have images of digits from 0-9 and each image here is of 784 by 1 tensor as MNIST digits are 28*28
 
 	x = tf.placeholder(tf.float32, [None, 784])
 	y_ = tf.placeholder(tf.float32,[None,10])
 	
+	###Enter code to define the necessary tensorflow variables, note that MNIST images are 28 * 28###
+	
+	#SOLUTION:
 	#The lines below create defines the variable weights and bias, weights depend on the the input tensors
 	#i.e., X and the b will depend on y as we need them to be the same size
 	W = tf.Variable(tf.zeros([784,10]))
 	b = tf.Variable(tf.zeros([10]))
 
+	###Enter the code for the usage of weights and the matrix multiplication. ###
+	
+	# SOLUTION:
 	#The matrix multiplication takes place here where we get the logits and use it later in softmax layer
-
 	y = tf.matmul(x,W) + b
 
+	### Enter the code to calculate the cross entropy loss.
+	# Use the tf.nn.softmax_cross_entropy_with_logits for that, then use tf.reduce_mean.
+	
+	# SOLUTION:
 	
 	#This step is to get the cross entropy loss with the softmax of logits,
 	#We use the tf.nn.softmax_cross_entropy_with_logits for that, then we use tf.reduce_mean to
 	#get the mean loss of the batch
 	cross_e = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels =y_,logits =y))
 	
+	### Enter Code: Minimize the loss with the tensorflow's gradient descent optimizer.
 	
+	#SOLUTION:
 	#This step is the gradient descent optimizer for loss minimization with appropriate learning rate
-	
 	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_e)
 	
 	#The line below launches the tensorflow session which will iteratively help us calculate the final accuracy
